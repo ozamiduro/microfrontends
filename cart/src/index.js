6b3b1b219d26befbbb287437c040356e012839e9
Ozami Duque Rodriguez <ozamiduro.dev@gmail.com>
2026-08-27 20:32:50 -0500

    feat: Add isolation & mount expose in Cart

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,7 +1,1 @@
-import faker from "faker"
-
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`
-
-
-const cartId = "dev-cart"
-document.querySelector(`#${cartId}`).innerHTML = cartText
+import('./bootstrap')
