ef7b666cb1e8744572e9e92830d9d33e3ae8b6f0
Ozami Duque Rodriguez <ozamiduro.dev@gmail.com>
2026-08-27 20:32:58 -0500

    feat: Add isolation & mount expose in Products

--- products/src/index.js
+++ products/src/index.js
@@ -1,12 +1,1 @@
-import faker from "faker"
-
-let products = ""
-
-for (let index = 0; index < 5; index++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`
-}
-
-const root = "dev-products"
-
-document.querySelector(`#${root}`).innerHTML = products
+import('./bootstrap')
